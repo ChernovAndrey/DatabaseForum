0a4dbf0ac2ffbee4a0dc29d2553fdf140aa07418
ChernovAndrey <chernov.andrey.98@mail.ru>
2017-03-12 14:27:34 +0300

    second commit

---
+++ src/main/java/master/objects/ObjThread.java
@@ -0,0 +1,108 @@
+package master.objects;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.json.JSONObject;
+
+/**
+ * Created by andrey on 11.03.17.
+ */
+public class ObjThread {
+    private Integer id;
+    private String title;
+    private String author;
+    private String forum;
+    private  String message;
+    private String slug;
+    private int votes;
+    private String created;
+    public ObjThread() {
+
+    }
+
+    @JsonCreator
+    public ObjThread(
+            @JsonProperty("id") Integer id,
+            @JsonProperty("title") String title,
+            @JsonProperty("author") String author,
+            @JsonProperty("slug") String slug,
+            @JsonProperty("message") String message,
+            @JsonProperty("forum") String forum,
+            @JsonProperty("votes") int votes,
+            @JsonProperty("created") String created) {
+        this.id = id;
+        this.title=title;
+        this.author = author;
+        this.forum=forum;
+        this.message=message;
+        this.slug = slug;
+        this.votes = votes;
+        this.created=created;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getForum() {return forum;}
+
+    public  String getMessage(){return message;}
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+    public String getCreated() {return created;}
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public  void setForum(String forum){this.forum=forum;}
+
+    public  void setMessage(String message){this.message=message;}
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+    public void setCreated(String created) {
+        this.created=created;
+    }
+
+
+    public JSONObject getJson() {
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", id);
+        jsonObject.put("title",title);
+        jsonObject.put("author", author);
+        jsonObject.put("forum",forum);
+        jsonObject.put("message",message);
+        jsonObject.put("slug", slug);
+        jsonObject.put("votes",votes);
+        jsonObject.put("created", created);
+        return jsonObject;
+    }
+}
---
+++ src/main/java/master/rowmaps/threadMapper.java
@@ -0,0 +1,25 @@
+package master.rowmaps;
+
+import master.objects.ObjThread;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by andrey on 11.03.17.
+ */
+public class threadMapper implements RowMapper<ObjThread>{
+    public ObjThread mapRow(ResultSet rs, int rowNum) throws SQLException {
+        ObjThread thread = new ObjThread();
+        thread.setId(rs.getInt("id"));
+        thread.setTitle(rs.getString("title"));
+        thread.setAuthor(rs.getString("author"));
+        thread.setForum(rs.getString("forum"));
+        thread.setMessage(rs.getString("message"));
+        thread.setVotes(rs.getInt("votes"));
+        thread.setSlug(rs.getString("slug"));
+        thread.setCreated(rs.getString("created"));
+        return thread;
+    }
+}
